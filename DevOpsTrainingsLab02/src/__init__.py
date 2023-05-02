167ac4c72f4010605db4c9bf3d161a52a4678b80
Erekle Magradze <erekle@magradze.de>
2023-05-02 17:15:20 +0400

    adding content

---
+++ DevOpsTrainingsLab02/src/__init__.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+def function_init():
+    print('Successfully Imported Init.py')
+    return 0
+
+def print_age(age):
+    return("I am %s years old"%str(age))
---
+++ DevOpsTrainingsLab02/test.py
@@ -0,0 +1,7 @@
+from DevOpsTrainingsLab02.__init__ import *
+
+def test_function_init():
+    assert function_init() == 0
+
+def test_print_age():
+    assert "I am 38 years old" in print_age(38)
